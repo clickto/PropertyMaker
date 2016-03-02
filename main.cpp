7cee623caaad7e8e8fcdcdbbca8767a867669278
Dick.jia <wentaojia2014@163.com>
2016-03-02 14:38:01 +0800

    ver 2 增加了 m_dptr方式

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ void addNode(QString type, QString name)
 	list.append(str);
 }
 
-void make()
+//常规的.成员在class 内
+void makecommon()
 {
 	QFile file("out.txt");
 	if (!file.open(QIODevice::ReadWrite | QIODevice::Append)) {
@@ -67,6 +68,46 @@ void make()
 	file.close();
 }
 
+//成员在m_dptr指向的类中
+void makeprivate()
+{
+	QFile file("out.txt");
+	if (!file.open(QIODevice::ReadWrite | QIODevice::Append)) {
+		return ;
+	}
+	QTextStream out(&file);
+
+	//定义部分
+	out <<QString("//%1.h").arg(classname)<<endl;
+
+	//实现部分
+	out <<QString("//%1.cpp").arg(classname)<<endl;
+
+//	int PWParams::audio () const
+//	{
+//		QScriptValue ret = m_dptr->engine->evaluate("audio()");
+//		if (ret.isNumber()) return ret.toInt32();
+//		return 0;
+//	}
+//	void PWParams::setAudio (int _value)
+//	{
+//		if ( audio() == _value)
+//			return;
+//		m_dptr->audio.setAttribute("value", _value);
+//		emit audioChanged();
+//	}
+
+	foreach(QStringList node, list) {
+		QString type = node.at(0);
+		QString name = node.at(1);
+		out <<QString("%1 %2::%3 () const\n{\n	QScriptValue ret = m_dptr->engine->evaluate(\"%4()\");\n	if (ret.isNumber())\n	return ret.toInt32();\n return 0;\n}").arg(type).arg(classname).arg(name).arg(name) <<endl;
+		out << QString ("void %1::set%2%3 (%4 _value)\n{\n if ( %5() == _value)\n	return;\n m_dptr->%5.setAttribute(\"value\", _value);\n emit %5Changed();\n}").arg(classname).arg(name.at(0).toUpper()).arg(name.mid(1, name.size())).arg(type).arg(name)<< endl;
+	}
+
+
+	file.close();
+}
+
 int main(int argc, char * argv[])
 {
 	setClassName("PWParams");
@@ -84,7 +125,7 @@ int main(int argc, char * argv[])
 	addNode("int", "resolution");
 	addNode("int", "triplex");
 
-	make();
+	makeprivate();
 
 	return 0;
 }
